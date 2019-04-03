d1243def9a453e9f67f124dff14457f018a1983a
Guido Rendon <grendon@live.com>
2019-04-03 11:18:04 -0500

    Added file size control up to 1MB and sorted.

--- challenge.sh
+++ challenge.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-#1
-rm test
+# Start the output file in empty
+> outputrandom
 
-A=0
-until [ $A -gt 10 ]; do
-	echo `cat /dev/urandom | tr -cd a-zA-Z0-9 | head -c15` >> test
-	A=$(( A+1 ))
+# Get the file size in bytes of the output file "outputrandom" with du, with awk
+# gets the first column of the du output
+filesize=`du -b outputrandom | awk {'print $1'}`
+# Can get the file size with ls command
+# filesize=`ls -l outputrandom | awk {'print $5'}`
+
+# --2--
+# Check the filesize to reach up to 1024KB = 1MB = 1048576B
+until [ $filesize -ge 1048576 ]; do
+	# --1--
+	# urandom generates a random characters, with tr and options -cd complements
+	# the filter of only characters from a-z, A-Z and 0-9 characters, with head
+	# up to 15 bytes, every character is a byte and tee apprends write the 15
+	# random chars to outputrandom file.
+	echo `cat /dev/urandom | tr -cd a-zA-Z0-9 | head -c15` | tee -a outputrandom
+	# Get the file size in bytes before the append to check if it is greather or equal to 1MB
+	filesize=$((A+16))
+	#filesize=`du -b test | awk {'print $1'}`
 done
+
+# --3--
+# File sorted by default in ascendent order, it start with numbers, followed by
+# letters from A to Z in upper case and later in lower case. With option -f can
+# ignore the case.
+sort outputrandom -o outputrandom
+
